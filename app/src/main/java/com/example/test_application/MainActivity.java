adf770d76e33b10ec4d5cf3f9a41408b8187d7c1
Rina <marianna.korolek.98@gmail.com>
2020-06-28 14:47:26 +0300

    Programming languages' list; xml changes (textColor, textSize, padding, added ScrollView, ...)

--- app/src/main/java/com/example/test_application/MainActivity.java
+++ app/src/main/java/com/example/test_application/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.test_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = findViewById(R.id.tv_list);
+        String[] names = {"Java", "Python", "C++", "Matlab", "Go", "C", "Swift", "JavaScript", "Ruby", "PHP", "Perl", "C#"};
+
+        list.setText("");
+        for(String name:names){
+            list.append(name + "\n");
+        }
     }
 }
